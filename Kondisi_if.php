963bdd7f86178665c97276a4ca922b0c1174b798
Syukur yakub <56242226+Syukuryakub@users.noreply.github.com>
2021-05-11 11:05:06 +0530

    Add files via upload

---
+++ Kondisi_if.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Dasar</title>
+</head>
+<body>
+    <h1>Kondisi if</h1>
+    <?php
+$nama_hari = date("l");
+    if ($nama_hari == "Sunday") {
+    echo "Minggu";
+} elseif ($nama_hari == "Monday") {
+    echo "Senin";
+} else {
+     echo "Selasa";
+}
+?>
+<h2>Kondisi Switch</h2>
+<?php
+$nama_hari = date("l");
+switch ($nama_hari) {
+ case "Sunday":
+ echo "Minggu";
+ break;
+ case "Monday":
+ echo "Senin";
+ break;
+ case "Tuesday":
+    echo "Selasa";
+    break;
+    default:
+    echo "Sabtu";
+   ?>
+</body>
+</html>
+
+
+   
